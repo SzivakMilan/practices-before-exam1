7b7e1630672e715f1cafcdb549a55cc8a01aa89e
SzivakMilan <milanszivak@gmail.com>
2023-11-21 19:42:04 +0100

    finish

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector("#root").insertAdjacentHTML("beforeend",`
+<form>
+    <h1>Register your pet!</h1>
+
+    <input type="text" placeholder="pet name">
+
+    <textarea placeholder="symptoms..."></textarea>
+
+    <button>submit</button>
+<div>
+    <input type="checkbox">
+    <p>elolvastam es elfogadom</p>
+</div>
+</form>
+
+<div>?</div>
+`)
